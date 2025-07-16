262889e12d4f350d58a08437ad3ff6ad58a6e1fe
Mikreselasie <abiymikreselasie21@gmail.com>
2025-07-16 15:56:39 +0300

    Add Update Page is Started

---
+++ mobile/mikreselasie-abiy/task_6/lib/add_update_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AddUpdatePage extends StatefulWidget {
+  const AddUpdatePage({super.key});
+
+  @override
+  State<AddUpdatePage> createState() => _AddUpdatePageState();
+}
+
+class _AddUpdatePageState extends State<AddUpdatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(children: []),
+      ),
+    );
+  }
+}
