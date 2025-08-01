6e511e1bb4aefe8aab7bb86e68ec7d91b9360813
Mikreselasie <abiymikreselasie21@gmail.com>
2025-08-02 00:12:03 +0300

    Task 13: Network info implementation is done

--- mobile/mikreselasie-abiy/tasks/lib/core/platform/network_info.dart
+++ mobile/mikreselasie-abiy/tasks/lib/core/platform/network_info.dart
@@ -1,3 +1,14 @@
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+
 abstract class NetworkInfo {
   Future<bool> get isConnected;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final InternetConnection connetctionChecker;
+
+  NetworkInfoImpl(this.connetctionChecker);
+
+  @override
+  Future<bool> get isConnected => connetctionChecker.hasInternetAccess;
 }
---
+++ mobile/mikreselasie-abiy/tasks/test/core/fixtures/platform/network_info_test.dart
@@ -0,0 +1,38 @@
+// test/core/network/network_info_test.dart
+
+import 'package:ecommerce/core/platform/network_info.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'network_info_test.mocks.dart';
+
+@GenerateMocks([InternetConnection])
+void main() {
+  late MockInternetConnection mockInternetConnection;
+  late NetworkInfoImpl networkInfo;
+
+  setUp(() {
+    mockInternetConnection = MockInternetConnection();
+    networkInfo = NetworkInfoImpl(mockInternetConnection);
+  });
+
+  group('isConnected', () {
+    test('should return true when InternetConnection has access', () async {
+      // arrange
+      final tHasConnectionFuture = Future.value(true);
+
+      when(
+        mockInternetConnection.hasInternetAccess,
+      ).thenAnswer((_) => tHasConnectionFuture);
+
+      // act
+      final result = networkInfo.isConnected;
+
+      // assert
+      verify(mockInternetConnection.hasInternetAccess);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
